1df342ebcfcba05e1ba912cfe543e9698e98c5f4
Jennifer Wong <jennwong1009@gmail.com>
2019-11-08 11:54:26 -0800

    process.env.NODE_ENV

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,8 +3,17 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== "production") {
+    // must use 'require' (import only allowed at top of file)
+    const { logger } = require("redux-logger");
+    middlewares.push(logger);
+}
+
+
 const configureStore = (preloadedState = {}) => {
-    return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger));
+    return createStore(RootReducer, preloadedState, applyMiddleware(...middlewares));
 };
 
 export default configureStore;
