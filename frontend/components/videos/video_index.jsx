f451ce0992c06c6a13381ade4babb6a94df020f7
Jennifer Wong <jennwong1009@gmail.com>
2019-12-27 00:30:40 -0800

    upload styling

--- frontend/components/videos/video_index.jsx
+++ frontend/components/videos/video_index.jsx
@@ -4,7 +4,6 @@ import VideoIndexItemContainer from './video_index_item_container';
 class VideoIndex extends React.Component {
     constructor(props){
         super(props);
-        debugger
         this.state = {
             isArray: Array.isArray(this.props.videos)
         }
@@ -15,6 +14,7 @@ class VideoIndex extends React.Component {
                 this.setState({ isArray: Array.isArray(this.props.videos)})
             })
         } else {
+            this.word = this.props.videos;
             this.props.fetchVideosWithQuery(this.props.videos).then(() => {
                 this.setState({ isArray: Array.isArray(this.props.videos) })
             })
@@ -23,13 +23,26 @@ class VideoIndex extends React.Component {
 
     render() {
         let display = null;
+        let searchResult = null;
         if(!this.state.isArray || (this.state.isArray && this.props.videos.length === 0)){
             display = (
                 null
             )
-        } else if(this.state.isArray){
+        } else if (this.word && (this.state.isArray && this.props.videos.length === 0)){
             display = (
-                <>
+                <header className="not-found">
+                    <h2>Sorry, videos not found</h2>
+                </header>
+            )
+        } else if(this.state.isArray){
+            // if(this.word) {
+            //     searchResult = (
+            //         <h2>Search results for "{this.word}"</h2>
+            //     )
+            // }
+            
+            display = (
+                <>  
                     {this.props.videos.map((video, id) => (
                         <div className="clip-div" key={id}>
                             <VideoIndexItemContainer
@@ -45,6 +58,9 @@ class VideoIndex extends React.Component {
         
         return (
             <div className="video-index-content">
+                {/* <div>
+                    {searchResult}    
+                </div> */}
                 {display}
             </div>
         )
