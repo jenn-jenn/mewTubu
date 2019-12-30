e01337a329470e49c5322d107d291adfec03876b
Jennifer <jennwong1009@gmail.com>
2019-12-29 18:12:35 -0800

    temp logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,7 +3,7 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
 
-const middlewares = [thunk];
+const middlewares = [thunk, logger];
 
 if (process.env.NODE_ENV !== "production") {
     // must use 'require' (import only allowed at top of file)
